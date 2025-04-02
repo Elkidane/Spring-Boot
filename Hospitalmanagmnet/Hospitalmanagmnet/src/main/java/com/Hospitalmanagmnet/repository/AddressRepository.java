56894c05df5081ec4ba34c3aeea1cb47d5b9cf35
elshaday kidane <elshadaykidane4@gmail.com>
2025-04-03 02:32:58 +1100

    Delete Hospitalmanagmnet directory

--- Hospitalmanagmnet/Hospitalmanagmnet/src/main/java/com/Hospitalmanagmnet/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Hospitalmanagmnet.repository;
-
-import com.Hospitalmanagmnet.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long>{
-    void deleteByPatientId(Long patientId);
-
-}
--- Hospitalmanagmnet/Hospitalmanagmnet/src/main/java/com/Hospitalmanagmnet/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Hospitalmanagmnet.repository;
-
-import com.Hospitalmanagmnet.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-}
